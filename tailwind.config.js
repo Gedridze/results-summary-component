667380f20843547c863875cbb736f4db3e098ff7
Edvinas <edvinas.kilciauskas@gmail.com>
2023-04-14 12:05:06 +0300

    add result widget mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./**/*.{html,js}'],
   theme: {
+    colors: {
+      white: "#ffffff",
+      "pale-blue": "#ebf1ff",
+      "light-lavender": "#c8c7ff",
+      "dark-grey-blue": "#303b5a",
+      red: "#ff5757",
+      orange: "#ffb01f",
+      green: "#00bd91",
+      blue: "#1125d4",
+      "grad-sl-blue": "#7857ff",
+      "grad-rl-blue": "#2e2be9",
+      "grad-v-blue": "#4e21ca",
+      "grad-p-blue": "#2421ca",
+    },
+    fontFamily: {
+      sans: ["Hanken Grotesk", "sans"]
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
